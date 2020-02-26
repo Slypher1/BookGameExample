b88953c0a1b7c16485e73d5847a4e82b3d0c7f68
Slypher <slypher@example.com>
2020-02-26 10:14:53 +0100

    todo list conclusa. Challenge completata

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Dati_sdlow209\Programmi\Flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Dati_sdlow209\Programmi\flutterApp\destini-challenge-starting"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\Dati_sdlow209\Programmi\Flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
